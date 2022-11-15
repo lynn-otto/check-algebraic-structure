58fe6e9415242ad5d60fea96448a38bce8d77fed
philipp-otto <97127286+philipp-otto@users.noreply.github.com>
2022-11-15 15:26:01 +0100

    Fix grammar

--- check_algebraic_structure.py
+++ check_algebraic_structure.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
     print(f'The multiplication table together with the element {neutral_element} is:')
     print(f'- {print_adjective_truthy(commutativity, "commutative")}')
     print(f'- {print_adjective_truthy(associativity, "assoicative")}')
-    print(f'- {neutral_element} is {print_adjective_truthy(associativity, "an neutral element")}')
+    print(f'- {neutral_element} is {print_adjective_truthy(associativity, "a neutral element")}')
     print(f'- {print_adjective_truthy(inverses, "every element has an inverse")}')
     linebreak()
 
